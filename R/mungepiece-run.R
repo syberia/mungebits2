33ea4110cec9362e127c7d907d8382880172d18a
RobertZK <technoguyrob@gmail.com>
2015-09-13 07:51:41 -0500

    Begin literate documentation of mungepiece

--- R/mungepiece-run.R
+++ R/mungepiece-run.R
@@ -1,1 +1,16 @@
+## Running a mungepiece respects the same laws as running a mungebit.
+## During training, the goal is to record the necessary metadata the
+## mungebit needs in order to run during prediction (i.e., on one
+## row data sets in a real-time production environment).
+##
+## The first time `mungepiece$run` is called, the call is delegated
+## to the `mungebit` attached to the mungepiece with the appropriate
+## training arguments. 
+##
+## For example, imagine we have a mungebit that discretizes a variable.
+##
+## ```r
+## 
+## ```
 mungepiece_run <- NULL
+
