422ba42fb7010e8fee1105ef8de3725ab475f4ea
RobertZK <technoguyrob@gmail.com>
2015-09-27 20:37:34 -0500

    Test that a simple munger works.

--- tests/testthat/test-munge.R
+++ tests/testthat/test-munge.R
@@ -4,3 +4,16 @@ test_that("it does nothing when no mungepieces are passed", {
   expect_equal(munge(iris, list()), iris[,])
 })
 
+test_that("it correctly adds to the mungepieces list", {
+  args <- lapply(seq_len(2),
+    function(.) list(column_transformation(function(x, one) x + one), 1))
+  iris2 <- munge(iris, args)
+  expect_equal(length(attr(iris2, 'mungepieces')), 2)
+})
+
+test_that("it correctly munges a simple mungepiece sequence", {
+  args <- lapply(seq_len(2) + 1,
+    function(x) list(function(x, v) { x[[1]] <- v * x[[1]]; x }, x))
+  iris2 <- munge(iris, args)
+  expect_equal(iris2[[1]], iris[[1]] * 6)
+})
