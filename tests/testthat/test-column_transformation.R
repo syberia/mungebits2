1e1156f363a5c40dd7d1328d4919b77e0375633d
RobertZK <technoguyrob@gmail.com>
2015-10-04 18:33:54 -0500

    Test a doubler column transformation.

--- tests/testthat/test-column_transformation.R
+++ tests/testthat/test-column_transformation.R
@@ -6,3 +6,10 @@ describe("Simplest examples", {
   })
 })
 
+test_that("correctly transforms one column by multiplying by two", {
+  doubler <- mungebit$new(column_transformation(function(x) { 2 * x }))
+  iris2   <- doubler$run(iris, "Sepal.Length")
+  expect_equal(iris2[TRUE], transform(iris, Sepal.Length = 2 * Sepal.Length),
+               info = "column_transformation must double first column of iris2")
+})
+
