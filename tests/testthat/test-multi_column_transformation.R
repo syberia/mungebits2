b98e27a9511bfe0a1bcdfb52083a1b0891ea0a38
RobertZK <technoguyrob@gmail.com>
2015-10-19 08:23:35 -0500

    Check that divider multi column transform works.

--- tests/testthat/test-multi_column_transformation.R
+++ tests/testthat/test-multi_column_transformation.R
@@ -5,5 +5,10 @@ describe("Simplest examples", {
     expect_equal(mungebit$new(multi_column_transformation(identity))$run(iris, "Sepal.Length", "Sepal.Length"), iris)
   })
 
+  test_that("it can run a divider", {
+    expect_equal(mungebit$new(multi_column_transformation(`/`))$run(iris, c("Sepal.Length", "Sepal.Width"), "Sepal.Ratio"),
+                 transform(iris, Sepal.Ratio = Sepal.Length / Sepal.Width))
+  })
+
 })
 
