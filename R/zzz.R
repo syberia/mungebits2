a1ea856c078344be1948b4d4d205fe15c50752e6
RobertZK <technoguyrob@gmail.com>
2015-10-26 09:29:14 -0500

    Warn the user when mungebits is loaded after mungebits2

--- R/zzz.R
+++ R/zzz.R
@@ -10,5 +10,16 @@
   }
 }
 
+.onAttach <- function(libname, pkgname) {
+  setHook(packageEvent("mungebits", "attach"), function(...) {
+    packageStartupMessage(crayon::red$bold(paste0(
+      "You have loaded mungebits after mungebits2 - ",
+      "this is likely to cause problems.\nIf you need functions from both ",
+      "mungebits and mungebits2 (which is unlikely), please load mungebits first, ",
+      "then mungebits2:\nlibrary(mungebits); library(mungebits2)")))
+  })
+}
+
+
 globalVariables(c("newpieces", "mungepieces", "size"))
 
