22ad2c10bd3a0538752cd6ec966c10fd49bf8541
RobertZK <technoguyrob@gmail.com>
2015-10-26 13:11:59 -0700

    Check that the zzz hooks dont error

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,4 +1,5 @@
 context("utils")
+library(testthatsomemore)
 
 describe("list2env_safe", {
   test_that("it works with 0 element lists", {
@@ -7,6 +8,16 @@ describe("list2env_safe", {
 
   test_that("it works with 2 element lists", {
     expect_equal(length(list2env_safe(list(a = 1, b = 2))), 2) 
+  })
+})
+
+describe("zzz", {
+  test_that("it can call onLoad", {
+    assert(suppressPackageStartupMessages(.onLoad()))
+  })
+
+  test_that("it can call onAttach", {
+    assert(suppressPackageStartupMessages(.onAttach()))
   })
 })
 
