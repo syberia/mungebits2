5e7a7520fb8b0ede6eeb3887da9ee492a52a57bc
RobertZK <technoguyrob@gmail.com>
2015-10-26 11:46:56 -0700

    Check it can print a split mungebit

--- tests/testthat/test-printing.R
+++ tests/testthat/test-printing.R
@@ -5,5 +5,14 @@ describe("printing mungepieces", {
     expect_output(print(mungepiece$new(mungebit$new())), "train and predict")
     expect_output(print(mungepiece$new(mungebit$new())), "untrained")
   })
+
+})
+
+describe("printing mungebits", {
+  test_that("it can print a split mungebit", {
+    expect_output(print(mungebit$new(function(x) 2 * x + 1, NULL)), "train function")
+    expect_output(print(mungebit$new(function(x) 2 * x + 1, NULL)),
+                  "function (x) 2 * x + 1", fixed = TRUE)
+  })
 })
 
