1a7d91b977ee2721d332fd4da39e161cb82ea9b3
RobertZK <technoguyrob@gmail.com>
2015-10-26 09:30:13 -0500

    Remove outdated TODO

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,3 @@
-# TODO: (RK) Add notice if mungebits and mungebits2 are
-# loaded at the same time.
-
 .onLoad <- function(libPath, pkg) {
   if (as.package_version(R.version) < as.package_version("3.1.0")) {
     packageStartupMessage(crayon::red(paste0(
