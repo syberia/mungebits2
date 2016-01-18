e169a03dcb2a61b51cf263a3d94a952a101a6004
RobertZK <technoguyrob@gmail.com>
2016-01-18 16:39:32 -0600

    Add failing test for column transformation dropper

--- tests/testthat/test-column_transformation.R
+++ tests/testthat/test-column_transformation.R
@@ -138,6 +138,14 @@ describe("Passing arguments", {
 
 })
 
+describe("dropping columns", {
+  test_that("it can drop columns using a column_transformation", {
+    dropper <- mungebit$new(column_transformation(function(x) NULL))
+    browser()
+    expect_equal(dropper$run(iris, 1), iris[-1])
+  })
+})
+
 if (requireNamespace("microbenchmark", quietly = TRUE)) {
   describe("Benchmarks", {
 
@@ -183,4 +191,5 @@ describe("debugging", {
     expect_false(isdebugged(get("transformation", envir = environment(ct))))
   })
 })
+
 
